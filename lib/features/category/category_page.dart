672588d4685315977bb5838f019ccecc9f95dde2
hafsaasaleem <shaikhhafsa215@gmail.com>
2022-06-16 12:21:39 +0500

    third commit

--- lib/features/category/category_page.dart
+++ lib/features/category/category_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:ninecoin/assets/assets.dart';
+import 'package:ninecoin/colors/colors.dart';
+import 'package:ninecoin/features/home/components/category_circular_card.dart';
 
 class CategoryPage extends StatelessWidget {
   static Route rout() {
@@ -9,9 +12,44 @@ class CategoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Category"),
+    return Scaffold(
+      appBar: AppBar(title: Text("Categories")),
+      body: Wrap(
+        alignment: WrapAlignment.center,
+        spacing: 32,
+        runSpacing: 20,
+        children: [
+          CategoryCircularCard(
+            onTap: () {},
+            imageUrl: Assets.salon,
+            color: CoinColors.green,
+            label: "A'Salon",
+          ),
+          CategoryCircularCard(
+            onTap: () {},
+            imageUrl: Assets.beautyIcon,
+            color: CoinColors.red12,
+            label: "Beauty",
+          ),
+          CategoryCircularCard(
+            onTap: () {},
+            imageUrl: Assets.electronic,
+            color: CoinColors.orange12,
+            label: "Electronic",
+          ),
+          CategoryCircularCard(
+            onTap: () {},
+            imageUrl: Assets.groceries,
+            color: CoinColors.yellow,
+            label: "Groceries",
+          ),
+          CategoryCircularCard(
+            onTap: () {},
+            imageUrl: Assets.sports,
+            color: CoinColors.blueAccent,
+            label: "Sports",
+          ),
+        ],
       ),
     );
   }
--- lib/features/notification/notifications_page.dart
+++ lib/features/notification/notifications_page.dart
@@ -1,4 +1,5 @@
 import 'package:badges/badges.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ninecoin/colors/colors.dart';
 import 'package:ninecoin/typography/text_styles.dart';
@@ -14,17 +15,21 @@ class NotificationPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: CoinColors.black12,
         centerTitle: true,
-        title: Text("Notification", style: CoinTextStyle.title1),
+        title: Text(
+          "Notification",
+          style: CoinTextStyle.title1.copyWith(fontWeight: FontWeight.w500),
+        ),
       ),
-      body: Column(
-        children: [
-          Container(
-            child: ListTile(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            ListTile(
               leading: CircleNotificationIcon(
                   icon: Badge(
-                    position: BadgePosition.topEnd(top: -17, end: -5),
-                    badgeContent: const Text(''),
+                    position: BadgePosition.topEnd(top: -4),
                     child: const Icon(
                       Icons.notifications,
                       color: Colors.white,
@@ -34,32 +39,105 @@ class NotificationPage extends StatelessWidget {
                   color: CoinColors.orange),
               title: Text(
                 "Yonqed SDN. BHD.",
-                style: CoinTextStyle.title2,
+                style:
+                    CoinTextStyle.title4.copyWith(fontWeight: FontWeight.w800),
               ),
               subtitle: Text(
                 "Mauris non ligul tempus lacinia velit.",
-                style: CoinTextStyle.title4,
+                style: CoinTextStyle.title5,
               ),
-              trailing: Text("28/4/2022"),
+              trailing: const Text("28/4/2022"),
             ),
-          )
-        ],
+            const Divider(
+              thickness: 2,
+            ),
+            ListTile(
+              leading: CircleNotificationIcon(
+                  icon: Badge(
+                    position: BadgePosition.topEnd(top: -4),
+                    child: Image.asset(
+                      "assets/icons/coupon.png",
+                      height: 25,
+                      width: 25,
+                    ),
+                  ),
+                  color: CoinColors.purple),
+              title: Text(
+                "Sonyod SDN. BHD.",
+                style:
+                    CoinTextStyle.title4.copyWith(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text(
+                "Mauris non ligul tempus lacinia velit.",
+                style: CoinTextStyle.title5,
+              ),
+              trailing: const Text("24/4/2022"),
+            ),
+            const Divider(
+              thickness: 2,
+            ),
+            ListTile(
+              leading: CircleNotificationIcon(
+                  icon: Image.asset(
+                    "assets/icons/news.png",
+                    height: 25,
+                    width: 25,
+                  ),
+                  color: CoinColors.green),
+              title: Text(
+                "Apple Store",
+                style:
+                    CoinTextStyle.title4.copyWith(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text(
+                "Mauris non ligul tempus lacinia velit.",
+                style: CoinTextStyle.title5,
+              ),
+              trailing: const Text("22/4/2022"),
+            ),
+            const Divider(thickness: 3),
+            ListTile(
+              leading: CircleNotificationIcon(
+                  icon: Image.asset(
+                    "assets/icons/lucky-draw.png",
+                    height: 25,
+                    width: 25,
+                  ),
+                  color: CoinColors.blueAccent),
+              title: Text(
+                "SengQuen SDN. BHD.",
+                style:
+                    CoinTextStyle.title4.copyWith(fontWeight: FontWeight.w800),
+              ),
+              subtitle: Text(
+                "Mauris non ligul tempus lacinia velit.",
+                style: CoinTextStyle.title5,
+              ),
+              trailing: const Text("22/4/2022"),
+            ),
+            const Divider(
+              thickness: 2,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
 class CircleNotificationIcon extends StatelessWidget {
-  const CircleNotificationIcon(
-      {Key? key, required this.icon, required this.color})
+  const CircleNotificationIcon({Key? key, this.icon, required this.color})
       : super(key: key);
-  final Widget icon;
+  final Widget? icon;
   final Color color;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(8.0),
+      alignment: Alignment.center,
+      height: 40,
+      width: 40,
+      // padding: const EdgeInsets.all(8.0),
       decoration: BoxDecoration(
         shape: BoxShape.circle,
         color: color,
@@ -68,3 +146,5 @@ class CircleNotificationIcon extends StatelessWidget {
     );
   }
 }
+
+
